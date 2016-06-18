d3054d00983692556ba7793c6729f59595000712
jeffreybiles <bilesjeffrey@gmail.com>
2016-06-18 08:21:04 -0500

    get models from server

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return [
-      {name: 'Gordon Ramsay', isHere: true},
-      {name: 'Anthony Bourdain', isHere: false},
-      {name: 'Rachael Ray', isHere: true},
-      {name: 'Jamie Oliver', isHere: false},
-      {name: 'Guy Fieri', isHere: true}
-    ]
+    return this.store.findAll('chef')
   }
 });
