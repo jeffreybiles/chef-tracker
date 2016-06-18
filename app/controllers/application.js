b4cdc0bc0c2571aba989d206a9609062e279e815
jeffreybiles <bilesjeffrey@gmail.com>
2016-06-18 08:21:08 -0500

    save the chefs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,9 +4,11 @@ export default Ember.Controller.extend({
   actions: {
     makePresent(chef){
       Ember.set(chef, 'isHere', true)
+      chef.save()
     },
     makeAbsent(chef){
       Ember.set(chef, 'isHere', false)
+      chef.save()
     }
   }
 });
