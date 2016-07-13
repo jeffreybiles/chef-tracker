7abfeb17fec0058fe74b840d24ececfb804cec1f
jeffreybiles <bilesjeffrey@gmail.com>
2016-07-13 00:10:29 -0500

    With ember-power-select

--- app/controllers/chefs/chef/edit.js
+++ app/controllers/chefs/chef/edit.js
@@ -3,8 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(){
-      this.get('model').save();
-      this.transitionToRoute('chefs.chef', this.get('model.id'));
+      this.get('model.chef').save();
+      this.transitionToRoute('chefs.chef', this.get('model.chef.id'));
+    },
+    selectRestaurant(selection, component){
+      let chef = this.get('model.chef');
+      chef.set('restaurant', selection).save().then(()=>{
+        selection.save()
+      });
     }
   }
 });
