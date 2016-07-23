eb8bf0197c8ddb8ed2fc6ea408aad00c0ac6edad
jeffreybiles <bilesjeffrey@gmail.com>
2016-07-23 15:47:51 -0500

    Improved with ES6

--- app/helpers/round.js
+++ app/helpers/round.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 
-export function round(params/*, hash*/) {
-  let number = params[0];
-  let roundDecimal = params[1] || 2;
+export function round([number, inputDecimal]/*, hash*/) {
+  let roundDecimal = inputDecimal || 2;
   let tenPower = Math.pow(10, roundDecimal)
   return Math.round(number * tenPower)/tenPower
 }
