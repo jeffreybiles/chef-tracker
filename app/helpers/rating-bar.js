8cb456b6285cf1ba1ee25506beb2d2c0ae9356ec
jeffreybiles <bilesjeffrey@gmail.com>
2016-08-08 09:34:02 -0500

    add new hash param for color

--- app/helpers/rating-bar.js
+++ app/helpers/rating-bar.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
-export function ratingBar([rating, inputWidth]/*, hash*/) {
+export function ratingBar([rating, inputWidth], {color = 'blue'}) {
   let percentage = rating * 10;
   let width = inputWidth ? `${inputWidth}px;` : '100%';
   return Ember.String.htmlSafe(`<div class='rating-bar' style='width: ${width};'>
-                                  <span class="inner-bar" style='width: ${percentage}%;'>
+                                  <span class="inner-bar" style='width: ${percentage}%; background-color: ${color}'>
                                   </span>
                                 </div>`);
 }
