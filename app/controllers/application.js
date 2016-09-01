5ffe0130952755abf2fd27993862ebfdc77d29fd
jeffreybiles <bilesjeffrey@gmail.com>
2016-08-31 20:48:45 -0500

    Challenge 1.3- Total Students

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   chefsHere: Ember.computed.filterBy('model', 'isHere'),
+  chefStudents: Ember.computed.mapBy('model', 'numberStudents'),
+  totalStudents: Ember.computed.sum('chefStudents'),
   actions: {
     makePresent(chef){
       Ember.set(chef, 'isHere', true)
